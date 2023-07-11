56b2a0c400158568a830a4c99358720260142c0e
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-11 13:58:51 +0530

    z

--- dashboard/new.php
+++ dashboard/new.php
@@ -273,7 +273,15 @@
       <div class="row">
         <div class="col">
           <div class="card border-0" hint="container">
-
+                        <div class="input-group">
+                <div class="form-outline">
+                  <input type="search" id="form1" class="form-control" />
+                  <label class="form-label" for="form1">Search</label>
+                </div>
+                <button type="button" class="btn btn-primary">
+                  <i class="fas fa-search"></i>
+                </button>
+              </div>
            </div>
         </div>
       </div>
