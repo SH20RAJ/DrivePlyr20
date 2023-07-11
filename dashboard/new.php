b045b4c6969fad66b604a70dfe315fa6bd7531b9
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-11 14:03:42 +0530

    t4

--- dashboard/new.php
+++ dashboard/new.php
@@ -282,6 +282,22 @@
           </div>
         </div>
       </form>
+      <style>
+    .search-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+
+    .search-box {
+      width: 300px;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+  </style>4t
 
          </div>
       <!-- Footer -->
