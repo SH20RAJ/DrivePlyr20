541ac58640273c2cdca17d2730474307ea8510b7
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-11 13:25:31 +0530

    rf

--- dashboard/nav.php
+++ dashboard/nav.php
@@ -65,14 +65,16 @@
           <!-- Divider -->
           <hr class="my-3">
           <!-- Heading-->
-          <h6 class="navbar-heading p-0 text-muted">
-            <span class="docs-normal"><span class="nav-item">
-              <a class="nav-link" href="https://driveplyr.hashnode.dev/?ref=main" target="_blank">
-                <i class="ni ni-spaceship"></i>
-                <span class="nav-link-text">Documentations</span>
-              </a>
-</span></span>
-          </h6> 
+            <h6 class="navbar-heading p-0 text-muted">
+              <span class="docs-normal">
+                <span class="nav-item">
+                <a class="nav-link" href="https://driveplyr.hashnode.dev/?ref=main" target="_blank">
+                  <i class="ni ni-spaceship"></i>
+                  <span class="nav-link-text">Documentations</span>
+                </a>
+                </span>
+              </span>
+            </h6> 
           <!-- Navigation 
           <ul class="navbar-nav mb-md-3">
             <li class="nav-item">
