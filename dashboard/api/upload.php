7f6c90cb5ad91b58123357cb24ab659127065e2c
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-11 19:15:54 +0530

    ...

--- dashboard/api/upload.php
+++ dashboard/api/upload.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
+include '../../conn.php';
 if(!isset($_SESSION)){
     echo "Not Allowed";
-}
+} else {
+    
 // Get the form data
-$user = $_SESSION['user'];
+$user = 2;//$_SESSION['user'];
 $url = $_POST['url'];
 $hosting = $_POST['hosting'];
 $title = $_POST['title'];
@@ -12,14 +14,17 @@ $description = $_POST['description'];
 $allow_download = $_POST['allow_download'];
 
 // Connect to the database
-$db = new PDO('mysql:host=localhost;dbname=videos', 'root', '');
+//$db = new PDO('mysql:host=localhost;dbname=videos', 'root', '');
 
 // Insert the data into the database
 $sql = "INSERT INTO videos (user, url, hosting, title, description, allow_download) VALUES ('$user', '$url', '$hosting', '$title', '$description', $allow_download)";
-$stmt = $db->prepare($sql);
+$stmt = $conn->prepare($sql);
 $stmt->execute();
 
 // Redirect the user to the home page
 header('Location: /');
+
+}
+
 
 ?>
