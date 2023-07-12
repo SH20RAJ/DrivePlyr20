9f837258fb65c7b45a5103e6b3ae37e8d6d1aeb4
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-12 18:59:31 +0530

    gre

---
+++ dashboard/api/update-video.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include '../../conn.php';
+
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    echo "Not Allowed";
+    exit;
+}
+
+// Get the video ID from the URL parameter
+$id = $_GET['id'];
+
+// Fetch the video information from the database
+$sql = "SELECT * FROM videos WHERE id = $id and user = $_SESSION['id']";
+$result = $conn->query($sql);
+
+// Check if the video exists
+if ($result->num_rows === 0) {
+    echo "Video not found";
+    exit;
+}
+
+// Get the form data
+$url = $_POST['url'];
+$title = $_POST['title'];
+$description = $_POST['description'];
+$allowDownload = isset($_POST['allow_download']) ? 1 : 0;
+$posterURL = $_POST['poster_url'];
+
+// Update the video details in the database
+$updateSql = "UPDATE videos SET url = '$url', title = '$title', description = '$description', allow_download = $allowDownload, poster_url = '$posterURL' WHERE id = $id";
+if ($conn->query($updateSql) === TRUE) {
+    // Video details updated successfully
+    header('Location: ../edit.php?id=' . $id);
+} else {
+    echo "Error updating video details: " . $conn->error;
+}
+?>
