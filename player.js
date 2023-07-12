5a58c7bacb50acce9b364d6dd446a131da50b850
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-12 20:48:18 +0530

    cdc

--- player.js
+++ player.js
@@ -1,26 +1,25 @@
 // Get all the script tags with the 'data-type' attribute set to 'driveplyr'
-const scriptTags = document.querySelectorAll('script[data-type="driveplyr"]');
+const dp_scriptTags = document.querySelectorAll('script[data-type="driveplyr"]');
 
 // Iterate over each script tag
-scriptTags.forEach((scriptTag) => {
+dp_scriptTags.forEach((scriptTag) => {
   // Get the attributes from the script tag
-  const videoID = scriptTag.getAttribute('data-id');
-  const playerType = scriptTag.getAttribute('player');
-  const height = scriptTag.getAttribute('data-height');
-  const width = scriptTag.getAttribute('data-width');
+  const dp_videoID = scriptTag.getAttribute('data-id');
+  const dp_playerType = scriptTag.getAttribute('player');
+  const dp_height = scriptTag.getAttribute('data-height');
+  const dp_width = scriptTag.getAttribute('data-width');
+  const dp_playerid = scriptTag.getAttribute('data-id');
 
   // Create the iframe element
   const iframe = document.createElement('iframe');
-  iframe.setAttribute('src', `https://driveplyr.appspages.online/player.html?id=${videoID}&player=${playerType}`);
-  iframe.setAttribute('height', height);
-  iframe.setAttribute('width', width);
+  iframe.setAttribute('src', `https://driveplyr.appspages.online/player.html?id=${dp_videoID}&player=${dp_playerType}`);
+  iframe.setAttribute('height', dp_height);
+  iframe.setAttribute('width', dp_width);
 
   // Get the parent element of the script tag
-  const parentElement = scriptTag.parentNode;
+  const parentElement = document.getElementById('driveplyr'+dp_playerid);
 
   // Append the iframe to the parent element
   parentElement.appendChild(iframe);
 
-  // Remove the script tag
-  parentElement.removeChild(scriptTag);
 });
