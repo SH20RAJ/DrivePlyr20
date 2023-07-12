b9de0e752da90bbe8bf698e231c7bbdc767c03c8
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-12 18:08:51 +0530

    ik

--- dashboard/add.php
+++ dashboard/add.php
@@ -1,23 +1,20 @@
 <!DOCTYPE html>
 <html>
-
 <head>
   <?php 
   include 'head.php';
   ?>
   <script>
-  function checkVideoURL() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const videoURL = urlParams.get('video');
+    function checkVideoURL() {
+      const urlParams = new URLSearchParams(window.location.search);
+      const videoURL = urlParams.get('video');
 
-  if (videoURL) {
-    alert('Video URL: ' + videoURL);
-  }
-}
-
-</script>
+      if (videoURL) {
+        alert('Video URL: ' + videoURL);
+      }
+    }
+  </script>
 </head>
-
 <body>
   <!-- Sidenav -->
   <?php include 'nav.php' ?>
@@ -25,88 +22,20 @@
   <div class="main-content" id="panel">
     <!-- Topnav -->
     <?php include 'topnav.php' ?>
-    <!-- Header -->
-    <!-- Header 
-    <div class="header pb-6 d-flex align-items-center" style="min-height: 500px; background-image: url(https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
-       Mask 
-      <span class="mask bg-gradient-default opacity-8"></span>
-      Header container 
-      <div class="container-fluid d-flex align-items-center">
-        <div class="row">
-          <div class="col-lg-7 col-md-10">
-            <h1 class="display-2 text-white">Hello Jesse</h1>
-            <p class="text-white mt-0 mb-5">This is your profile page. You can see the progress you've made with your work and manage your projects or assigned tasks</p>
-            <a href="#!" class="btn btn-neutral">Edit profile</a>
-          </div>
-        </div>
-      </div>
-    </div>-->
     <!-- Page content -->
     <div class="container-fluid mt--6">
       <div class="row">
         <div class="col-xl-4 order-xl-2">
-          <!-- <div class="card card-profile">
-            <img src="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/img/theme/img-1-1000x600.jpg" alt="Image placeholder" class="card-img-top">
-            <div class="row justify-content-center">
-              <div class="col-lg-3 order-lg-2">
-                <div class="card-profile-image">
-                  <a href="#">
-                    <img src="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/img/theme/team-4.jpg" class="rounded-circle">
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-              <div class="d-flex justify-content-between">
-                <a href="#" class="btn btn-sm btn-info  mr-4 ">Connect</a>
-                <a href="#" class="btn btn-sm btn-default float-right">Message</a>
-              </div>
-            </div>
-            <div class="card-body pt-0">
-              <div class="row">
-                <div class="col">
-                  <div class="card-profile-stats d-flex justify-content-center">
-                    <div>
-                      <span class="heading">22</span>
-                      <span class="description">Friends</span>
-                    </div>
-                    <div>
-                      <span class="heading">10</span>
-                      <span class="description">Photos</span>
-                    </div>
-                    <div>
-                      <span class="heading">89</span>
-                      <span class="description">Comments</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="text-center">
-                <h5 class="h3">
-                  Jessica Jones<span class="font-weight-light">, 27</span>
-                </h5>
-                <div class="h5 font-weight-300">
-                  <i class="ni location_pin mr-2"></i>Bucharest, Romania
-                </div>
-                <div class="h5 mt-4">
-                  <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
-                </div>
-                <div>
-                  <i class="ni education_hat mr-2"></i>University of Computer Science
-                </div>
-              </div>
-            </div>
-          </div> -->
-              <video width="100%" height="" controls>
-      <source src="https://bit.ly/bbsamplevideo" type="video/mp4">
-    </video>
+          <video width="100%" height="" controls>
+            <source src="https://bit.ly/bbsamplevideo" type="video/mp4">
+          </video>
         </div>
         <div class="col-xl-8 order-xl-1">
           <div class="card">
             <div class="card-header">
               <div class="row align-items-center">
                 <div class="col-8">
-                  <h3 class="mb-0">Edit profile </h3>
+                  <h3 class="mb-0">Edit profile</h3>
                 </div>
                 <div class="col-4 text-right">
                   <a href="#!" class="btn btn-sm btn-primary">Settings</a>
@@ -114,63 +43,58 @@
               </div>
             </div>
             <div class="card-body">
-            <form class="needs-validation" action="/upload-video" method="post" enctype="multipart/form-data">
-  <h6 class="heading-small text-muted mb-4">Video information</h6>
-  <div class="pl-lg-4">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="form-group">
-          <label class="form-control-label" for="input-url">Video URL</label>
-          <input id="input-url" class="form-control" placeholder="Video URL" name="url" value="" type="text" required>
-          <div class="invalid-feedback">
-            Please enter a valid video URL.
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-4">
-        <div class="form-group">
-          <label class="form-control-label" for="input-hosting">Hosting</label>
-          <input id="input-hosting" class="form-control" name="hosting" readonly placeholder="Do not edit">
-          </input>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="form-group">
-          <label class="form-control-label" for="input-title">Title</label>
-          <input type="text" id="input-title" class="form-control" placeholder="Title" name="title" value="" required>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="form-group">
-          <label class="form-control-label" for="input-description">Description</label>
-          <textarea rows="1" class="form-control" placeholder="Description" name="description" required></textarea>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-4">
-        <div class="form-group">
-          <label class="form-control-label" for="input-allow-download">Allow download</label>
-          <input type="checkbox" id="input-allow-download" class="form-control" name="allow_download" value="1">
-        </div>
-      </div>
-      <div class="col-lg-8">
-        <div class="form-group">
-          <label class="form-control-label" for="input-poster">Poster URL</label>
-          <input type="text" id="input-poster" class="form-control" placeholder="Poster URL" name="poster_url" value="">
-        </div>
-      </div>
-    </div>
-  </div>
-  <hr class="my-4"></hr>
-  <button class="btn btn-primary btn-lg btn-block" type="submit">Upload</button>
-</form>
- 
-            <div>
-
-
+              <form class="needs-validation" action="/upload-video" method="post" enctype="multipart/form-data">
+                <h6 class="heading-small text-muted mb-4">Video information</h6>
+                <div class="pl-lg-4">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <label class="form-control-label" for="input-url">Video URL</label>
+                        <input id="input-url" class="form-control" placeholder="Video URL" name="url" value="" type="text" required>
+                        <div class="invalid-feedback">
+                          Please enter a valid video URL.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-4">
+                      <div class="form-group">
+                        <label class="form-control-label" for="input-hosting">Hosting</label>
+                        <input id="input-hosting" class="form-control" name="hosting" readonly placeholder="Do not edit">
+                      </div>
+                    </div>
+                    <div class="col-lg-4">
+                      <div class="form-group">
+                        <label class="form-control-label" for="input-title">Title</label>
+                        <input type="text" id="input-title" class="form-control" placeholder="Title" name="title" value="" required>
+                      </div>
+                    </div>
+                    <div class="col-lg-4">
+                      <div class="form-group">
+                        <label class="form-control-label" for="input-description">Description</label>
+                        <textarea rows="1" class="form-control" placeholder="Description" name="description" required></textarea>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-4">
+                      <div class="form-group">
+                        <label class="form-control-label" for="input-allow-download">Allow download</label>
+                        <input type="checkbox" id="input-allow-download" class="form-control" name="allow_download" value="1">
+                      </div>
+                    </div>
+                    <div class="col-lg-8">
+                      <div class="form-group">
+                        <label class="form-control-label" for="input-poster">Poster URL</label>
+                        <input type="text" id="input-poster" class="form-control" placeholder="Poster URL" name="poster_url" value="">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <hr class="my-4"></hr>
+                <button class="btn btn-primary btn-lg btn-block" type="submit">Upload</button>
+              </form>
             </div>
           </div>
         </div>
@@ -188,6 +112,10 @@
   <script src="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
   <!-- Argon JS -->
   <script src="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/js/argon.js?v=1.2.0"></script>
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      checkVideoURL();
+    });
+  </script>
 </body>
-
 </html>
