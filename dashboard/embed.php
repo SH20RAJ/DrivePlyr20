0d10e925084915bf7bf8779300168bf2e04cd897
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-12 20:58:37 +0530

    ced

--- dashboard/embed.php
+++ dashboard/embed.php
@@ -3,21 +3,7 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
-  <meta name="author" content="Creative Tim">
-  <title>Argon Dashboard - Free Dashboard for Bootstrap 4</title>
-  <!-- Favicon -->
-  <link rel="icon" href="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/img/brand/favicon.png" type="image/png">
-  <!-- Fonts -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-  <!-- Icons -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/vendor/nucleo/css/nucleo.css" type="text/css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
-  <!-- Page plugins -->
-  <!-- Argon CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/css/argon.css?v=1.2.0" type="text/css">
+<?php include 'head.php' ?>
 </head>
 
 <body>
@@ -56,6 +42,7 @@
       <div class="row">
         <div class="col">
           <div class="card border-0">
+            <div id="driveplyr1"></div>
           <script src="https://driveplyr.appspages.online/player.js" data-id="1" player="videojs" data-height="400px" data-width="500px" data-type="driveplyr" defer></script>
         </div>
         </div>
