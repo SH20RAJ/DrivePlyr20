9742aa8effe742ff76433b80f3bfdac60ea4a1c6
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-12 18:01:43 +0530

    th

--- dashboard/add.php
+++ dashboard/add.php
@@ -2,21 +2,10 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
-  <meta name="author" content="Creative Tim">
-  <title>Argon Dashboard - Free Dashboard for Bootstrap 4</title>
-  <!-- Favicon -->
-  <link rel="icon" href="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/img/brand/favicon.png" type="image/png">
-  <!-- Fonts -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-  <!-- Icons -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/vendor/nucleo/css/nucleo.css" type="text/css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
-  <!-- Argon CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/argon-dashboard-bs4@main/assets/css/argon.css?v=1.2.0" type="text/css">
-<script>
+  <?php 
+  include 'head.php';
+  ?>
+  <script>
   function checkVideoURL() {
   const urlParams = new URLSearchParams(window.location.search);
   const videoURL = urlParams.get('video');
@@ -143,11 +132,11 @@
       <div class="col-lg-4">
         <div class="form-group">
           <label class="form-control-label" for="input-hosting">Hosting</label>
-          <select id="input-hosting" class="form-control" name="hosting">
+          <input id="input-hosting" class="form-control" name="hosting" readonly>
             <option value="youtube">YouTube</option>
             <option value="vimeo">Vimeo</option>
             <option value="other">Other</option>
-          </select>
+          </input>
         </div>
       </div>
       <div class="col-lg-4">
