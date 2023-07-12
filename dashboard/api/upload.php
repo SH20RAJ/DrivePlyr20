683e2464448d408ac8a1a7daa0ab5b2724c8e234
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-12 18:24:05 +0530

    ef

--- dashboard/api/upload.php
+++ dashboard/api/upload.php
@@ -1,30 +1,28 @@
 <?php
 session_start();
 include '../../conn.php';
-if(!isset($_SESSION['username'])){
+
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
     echo "Not Allowed";
 } else {
-    print_r($_SESSION);
-// Get the form data
-$user = $_SESSION['user'];
-$url = $_POST['url'];
-$hosting = $_POST['hosting'];
-$title = $_POST['title'];
-$description = $_POST['description'];
-$allow_download = $_POST['allow_download'];
+    // Get the form data
+    $user = $_SESSION['username'];
+    $url = $_POST['url'];
+    $hosting = $_POST['hosting'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $allow_download = isset($_POST['allow_download']) ? 1 : 0;
+    $poster_url = $_POST['poster_url'];
 
-// Connect to the database
-//$db = new PDO('mysql:host=localhost;dbname=videos', 'root', '');
+    // Insert the data into the database
+    $sql = "INSERT INTO videos (user, url, hosting, title, description, allow_download, poster_url) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssssds", $user, $url, $hosting, $title, $description, $allow_download, $poster_url);
+    $stmt->execute();
 
-// Insert the data into the database
-$sql = "INSERT INTO videos (user, url, hosting, title, description, allow_download) VALUES ('$user', '$url', '$hosting', '$title', '$description', $allow_download)";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-
-// Redirect the user to the home page
-header('Location: /');
-
+    // Redirect the user to the home page or a success page
+    header('Location: ../videos.php');
+    exit();
 }
-
-
 ?>
