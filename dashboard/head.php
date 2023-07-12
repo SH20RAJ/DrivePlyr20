80cbffb16b38daf2098b779844301dc5e09df18f
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-12 17:27:36 +0530

    z

--- dashboard/head.php
+++ dashboard/head.php
@@ -1,5 +1,5 @@
 
-<meta charset="utf-8">
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
