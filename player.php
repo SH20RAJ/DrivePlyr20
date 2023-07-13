009f75b7e368c0a94a8dbcbe7aafdb97e8b1897d
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 10:06:03 +0530

    csd

--- player.php
+++ player.php
@@ -1,9 +1,13 @@
 <?php
 
+$video = json_decode(file_get_contents('https://driveplyr.appspages.online/dashboard/api/video.php?id='.$_GET['id']));
+
+print_r($video);
 if(isset($_GET['player'])){
   $player = $_GET['player'];
 }
 $videourl = 'https://driveplyr.appspages.online/dashboard/api/getvideo.php?id='.$_GET['id'];
+$title = 
 include 'player/griffith.php';
 
 ?>
