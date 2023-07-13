3c1c6543e917e0b056fc679e8d30e610b34889cf
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 10:13:58 +0530

    wqd

--- player.php
+++ player.php
@@ -3,11 +3,11 @@
 $video = json_decode(file_get_contents('https://driveplyr.appspages.online/dashboard/api/video.php?id='.$_GET['id']));
 
 //print_r($video);
-$poster_url = $video['poster_url'];
-$title = $video['title'];
-$views = $video['views'];
-$downloads = $video['downloads'];
-$description = $video['description'];
+echo $poster_url = $video['poster_url'];
+echo $title = $video['title'];
+echo $views = $video['views'];
+echo $downloads = $video['downloads'];
+echo $description = $video['description'];
 
 //
 
