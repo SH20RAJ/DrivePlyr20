2849a2f1befaa224edde8f89f5b880d6e69ea076
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 13:25:30 +0530

    cx

--- player/ckin.php
+++ player/ckin.php
@@ -6,17 +6,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ckin — Custom HTML5 Video Player Skins.</title>
-    <link rel="stylesheet" href="dist/css/ckin.css">
-    <link rel="stylesheet" href="demoDist/demo.css">
-    <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-touch-icon.png">
+    <link rel="stylesheet" href="player/plyr.phpdist/css/ckin.css">
     <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/favicons/manifest.json">
-    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
-    <link rel="shortcut icon" href="/favicons/favicon.ico">
-    <meta name="msapplication-config" content="/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">
   
 </head>
 
