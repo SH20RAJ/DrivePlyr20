ef06a256a916f7f663a922785f8cb9028f15e662
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 14:55:14 +0530

    re

--- dashboard/login.php
+++ dashboard/login.php
@@ -90,7 +90,7 @@
           <div class="row justify-content-center">
             <div class="col-xl-5 col-lg-6 col-md-8 px-5">
               <h1 class="text-white">Welcome!</h1>
-              <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p>
+              <p class="text-lead text-white">Your DrivePlyr Dashboard is waiting...</p>
             </div>
           </div>
         </div>
