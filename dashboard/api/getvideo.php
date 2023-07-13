7d2170792331307498f6f3dc0b02d897bd5f193b
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 09:55:00 +0530

    2w

--- dashboard/api/getvideo.php
+++ dashboard/api/getvideo.php
@@ -63,7 +63,7 @@ if(isset($_GET['poster'])){header('Location: '.$video['poster_url'].'');exit();}
         //return 'archive.org';
     } 
 
-$query = 'UPDATE `videos` SET `views` = `views` + 1 WHERE `videos`.`id` = 1;';
+$query = 'UPDATE videos SET views = views + 1 WHERE id = ' $id .'';
 $result = $connection->query($query);
 
 header('Location: '.$url.'');
