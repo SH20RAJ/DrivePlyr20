0dfd370801c48304ab5fdcc43c45934ec98948d8
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 13:36:26 +0530

    dewd

--- player/kwgplayer.php
+++ player/kwgplayer.php
@@ -3,6 +3,23 @@
 <link rel="icon" href="../logo.png">
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/webgadgets/KwgVideoPlayer@master/kwg-video-player.css" />
 <script src="https://cdn.jsdelivr.net/gh/webgadgets/KwgVideoPlayer@master/kwg-video-player.js"></script>
+<style>
+  html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#video1 {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
+
 
 <video id="video1" poster="<?php echo $poster_url ?>">
     <source src="<?php echo $videourl ?>" type="video/mp4">
