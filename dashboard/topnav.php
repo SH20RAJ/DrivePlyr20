ca6a31a90abf5c17a1f51daa3c637d8ebfae7785
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 11:22:24 +0530

    swq

--- dashboard/topnav.php
+++ dashboard/topnav.php
@@ -2,13 +2,13 @@
       <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <!-- Search form -->
-          <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
+          <form action="add.php" class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
             <div class="form-group mb-0">
               <div class="input-group input-group-alternative input-group-merge">
                 <div class="input-group-prepend">
                   <span class="input-group-text"><i class="fas fa-search"></i></span>
                 </div>
-                <input class="form-control" placeholder="Search" type="text">
+                <input class="form-control" placeholder="Paste video link here..." type="text" name="url">
               </div>
             </div>
             <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main" aria-label="Close">
