ac7e6713fbb1cce369fae6b4b7812e742d2ffc32
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 12:53:42 +0530

    many new player created ...

---
+++ player/openplayerjs.php
@@ -0,0 +1,35 @@
+
+<title><?php echo $title ?></title>
+<link href="https://cdn.jsdelivr.net/gh/SH20RAJ/Sopplayer/sopplayer.min.css" rel="stylesheet" />
+  <!--Here is the Css Library-->
+<link rel="icon" href="../logo.png">
+<script src="https://rebrand.ly/SopPlayerJS" defer></script>
+
+<style>
+  html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#my-video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.vjs-poster {
+  background-color: transparent;
+}
+
+</style>
+
+    <video id="my-video" class="sopplayer" controls preload="auto" data-setup="{}" 
+    width= "100%" poster="<?php echo $poster_url ?>">
+      <!--Use class="sopplayer" and data-setup="{}" -->
+      <source src="<?php echo $videourl ?>" type="video/mp4" />
+    </video>
+
+    <!--Here is the JavaScript Library-->
