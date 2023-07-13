84a039998fefc1fe7cd5b44e5c19d73d1b40eec6
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 13:17:15 +0530

    ryyt

--- player/openplayerjs.php
+++ player/openplayerjs.php
@@ -1,7 +1,23 @@
 <link rel="icon" href="./logo.png" />
 <title><?php echo $title ?></title>
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/openplayerjs@latest/dist/openplayer.min.css" />
+ <style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 
+#player  {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  /*object-fit: cover;
+  z-index: -1;*/
+}
+</style>
 
   <video class="op-player__media" id="player" controls playsinline width="100%">
             <source src="<?php echo $videourl ?>" type="video/mp4" />
