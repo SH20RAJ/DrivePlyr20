c79274d55c9fdcbd3d06b78c7363a04f0721c606
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 10:19:19 +0530

    qz

--- player2.php
+++ player2.php
@@ -2,13 +2,15 @@
 
 $video = json_decode(file_get_contents('https://driveplyr.appspages.online/dashboard/api/video.php?id='.$_GET['id']));
 
-print_r($video);
-echo $poster_url = $video->poster_url;
-die();
-echo $title = $video['title'];
-echo $views = $video['views'];
-echo $downloads = $video['downloads'];
-echo $description = $video['description'];
+ $poster_url = $video->poster_url;
+ $title = $video['title'];
+ $views = $video['views'];
+ $downloads = $video['downloads'];
+ $description = $video['description'];
+ $videourl = 'https://driveplyr.appspages.online/dashboard/api/getvideo.php?id='.$_GET['id'];
 
+ if(isset($_GET['player'])){$player = $_GET['player'];}
+ include 'player/'.$player.'.php';
+  
 
 ?>
