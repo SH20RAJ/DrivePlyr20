8e36bd128e07c802292a05e70e9cbd53e6c3fe77
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 12:32:58 +0530

    32d2

---
+++ player/sopplayer.php
@@ -0,0 +1,30 @@
+
+<title></title>
+<link href="https://cdn.jsdelivr.net/gh/SH20RAJ/Sopplayer/sopplayer.min.css" rel="stylesheet" />
+  <!--Here is the Css Library-->
+<link rel="icon" href="../logo.png">
+
+<style>
+  html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#my-video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
+
+    <video id="my-video" class="sopplayer" controls preload="auto" data-setup="{}" width= "100%" poster="<?php echo $poster_url ?>">
+      <!--Use class="sopplayer" and data-setup="{}" -->
+      <source src="<?php echo $videourl ?>" type="video/mp4" />
+    </video>
+
+   <script src="https://rebrand.ly/SopPlayerJS"></script>
+    <!--Here is the JavaScript Library-->
