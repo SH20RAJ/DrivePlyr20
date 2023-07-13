4bbeac55427f3354e0e8138bc0c45d7a1e115c46
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 13:10:05 +0530

    ok

--- player/rainplayer.php
+++ player/rainplayer.php
@@ -6,8 +6,8 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="icon" href="./assets/favicon.svg" />
-  <link rel="stylesheet" href="./styles/style.css" />
+  <link rel="icon" href="./logo.png" />
+  <link rel="stylesheet" href="https://sh20raj.github.io/Sopplayer/rainplayer/styles/style.css" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;700&display=swap" rel="stylesheet" />
