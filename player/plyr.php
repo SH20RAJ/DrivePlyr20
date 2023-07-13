f04be6c0d9a3ade84a4078ce82c01d2589d037c3
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 10:24:02 +0530

    wede

--- player/plyr.php
+++ player/plyr.php
@@ -4,9 +4,21 @@
   <title><?php echo $title ?></title>
   <link rel="stylesheet" href="https://cdn.plyr.io/3.6.3/plyr.css">
   <script src="https://cdn.plyr.io/3.6.3/plyr.js"></script>
+  <style>
+    #player {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #000; /* Set the background color to your preference */
+  z-index: 9999; /* Adjust the z-index as needed */
+}
+
+  </style>
 </head>
 <body>
-  <div class="plyr__video-container">
+  <div id="player" class="plyr__video-container">
     <video class="plyr__video" controls>
       <source src="<?php echo $videourl ?>" type="video/mp4">
     </video>
