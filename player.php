f21171c7773e9fa952a862ae9a692f5a9c9775a4
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 10:09:09 +0530

    ed

--- player.php
+++ player.php
@@ -3,11 +3,20 @@
 $video = json_decode(file_get_contents('https://driveplyr.appspages.online/dashboard/api/video.php?id='.$_GET['id']));
 
 print_r($video);
+$poster_url = $video['poster_url'];
+$title = $video['title'];
+$views = $video['views'];
+$downloads = $video['downloads'];
+$description = $video['description'];
+
+
+//
+
 if(isset($_GET['player'])){
   $player = $_GET['player'];
 }
 $videourl = 'https://driveplyr.appspages.online/dashboard/api/getvideo.php?id='.$_GET['id'];
-$title = 
+
 include 'player/griffith.php';
 
 ?>
