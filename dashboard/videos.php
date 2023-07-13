2fca5834c515b4346a254e6030503969fded88c0
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 12:08:16 +0530

    dx

--- dashboard/videos.php
+++ dashboard/videos.php
@@ -75,7 +75,7 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $videoId = $row['id'];
         $videoTitle = $row['title'];
-        $videoPosterURL = $row['poster_url'] ?: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png';
+        $videoPosterURL = $row['poster_url'] ?: 'api/Image_not_available.png';
         $videoStatus = $row['status'];
         $videoViews = $row['views'];
         $videoDownloads = $row['downloads'];
