807ab7236a5930ea915e5b1c7ec9ceed2c410ad4
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 10:21:16 +0530

    cds

--- player2.php
+++ player2.php
@@ -3,14 +3,14 @@
 $video = json_decode(file_get_contents('https://driveplyr.appspages.online/dashboard/api/video.php?id='.$_GET['id']));
 
  $poster_url = $video->poster_url;
- $title = $video['title'];
- $views = $video['views'];
- $downloads = $video['downloads'];
- $description = $video['description'];
+ $title = $video->title;
+ $views = $video->views;
+ $downloads = $video->downloads;
+ $description = $video->description;
  $videourl = 'https://driveplyr.appspages.online/dashboard/api/getvideo.php?id='.$_GET['id'];
 
  if(isset($_GET['player'])){$player = $_GET['player'];}
- echo 'player/'.$player.'.php';
-  
+ include 'player/'.$player.'.php';
+ 
 
 ?>
