ef4b793c8aa0891a55514949ca7362bb318ae873
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 10:17:09 +0530

    dewq

--- player2.php
+++ player2.php
@@ -3,7 +3,8 @@
 $video = json_decode(file_get_contents('https://driveplyr.appspages.online/dashboard/api/video.php?id='.$_GET['id']));
 
 print_r($video);
-echo $poster = $video['poster_url'];
+echo $poster_url = $video->poster_url;
+die();
 echo $title = $video['title'];
 echo $views = $video['views'];
 echo $downloads = $video['downloads'];
