48bd63cadc92d1d29f95670f700c699e996bb9e8
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-13 13:14:41 +0530

    rtr

--- player/openplayerjs.php
+++ player/openplayerjs.php
@@ -1,35 +1,18 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/openplayerjs@latest/dist/openplayer.min.css" />
+<!-- Adding Css CDN -->
 
-<title><?php echo $title ?></title>
-<link href="https://cdn.jsdelivr.net/gh/SH20RAJ/Sopplayer/sopplayer.min.css" rel="stylesheet" />
-  <!--Here is the Css Library-->
-<link rel="icon" href="../logo.png">
-<script src="https://rebrand.ly/SopPlayerJS" defer></script>
 
-<style>
-  html, body {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
+  <video class="op-player__media" id="player" controls playsinline width="100%">
+            <source src="https://archive.org/download/sample-video_202306/SampleVideo.mp4" type="video/mp4" />
+  </video>
 
-#my-video {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
+<hr>
 
-.vjs-poster {
-  background-color: transparent;
-}
+<script src="https://cdn.jsdelivr.net/npm/openplayerjs@latest/dist/openplayer.min.js"></script>
+<!-- Adding JavaScript CDN -->
 
-</style>
-
-    <video id="my-video" class="sopplayer" controls preload="auto" data-setup="{}" 
-    width= "100%" poster="<?php echo $poster_url ?>">
-      <!--Use class="sopplayer" and data-setup="{}" -->
-      <source src="<?php echo $videourl ?>" type="video/mp4" />
-    </video>
-
-    <!--Here is the JavaScript Library-->
+<script>
+   // Check the `API and events` link below for more options
+    const player = new OpenPlayerJS('player');
+    player.init();
+</script>
