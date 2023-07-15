96872451be7a7d0199c53a5f326f253cdb476160
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-15 08:31:30 +0530

    Update drive.php

--- dashboard/api/drive.php
+++ dashboard/api/drive.php
@@ -4,21 +4,21 @@ function getGoogleDriveVideoUrl($url)
     $matches = [];
     $pattern = '/https:\/\/drive\.google\.com\/file\/d\/([a-zA-Z0-9_-]+)\/view/';
     preg_match($pattern, $url, $matches);
-    
+
     if (count($matches) >= 2) {
         $fileId = $matches[1];
         $apiUrl = "https://www.googleapis.com/drive/v3/files/{$fileId}?fields=webViewLink";
         $response = file_get_contents($apiUrl);
-        
+
         if ($response !== false) {
             $data = json_decode($response, true);
-            
+
             if (isset($data['webViewLink'])) {
                 return $data['webViewLink'];
             }
         }
     }
-    
+
     return null;
 }
 
@@ -27,9 +27,11 @@ $url = $_GET['URL'] ?? ''; // Retrieve the URL parameter
 
 if (!empty($url)) {
     $videoUrl = getGoogleDriveVideoUrl($url);
-    
+
     if (!is_null($videoUrl)) {
-        echo "Video URL: $videoUrl";
+        // Output the video as a video content type
+        header("Content-Type: video/mp4");
+        readfile($videoUrl);
     } else {
         echo "Invalid Google Drive URL or unable to retrieve video URL.";
     }
