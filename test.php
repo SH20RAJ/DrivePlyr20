003271a4457b926938f4128b451f610e5a0a8fef
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-16 10:24:48 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,19 +1,5 @@
 <?php
-$videoUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/mp4/BigBuckBunny.mp4";
+$videoUrl = "https://downloadmovie1click.online/Transformers.Rise.Of.The.Beasts.2023.mp4";
 
-// Set the appropriate content type header
 header('Content-Type: video/mp4');
-
-// Get the file size
-$fileSize = filesize($videoUrl);
-
-// Set the content length header
-header("Content-Length: $fileSize");
-
-// Proxy the video file
-$fp = fopen($videoUrl, 'rb');
-while (!feof($fp)) {
-    echo fread($fp, 8192);
-    flush();
-}
-fclose($fp);
+readfile($videoUrl);
