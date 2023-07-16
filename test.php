a36f8ba3cd9e0929e4c59ec04132779ca430ac24
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-16 10:28:42 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,18 @@
 <?php
 $videoUrl = "https://downloadmovie1click.online/Transformers.Rise.Of.The.Beasts.2023.mp4";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $videoUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_HEADER, 0);
+$out = curl_exec($ch);
+curl_close($ch);
 
-header('Content-Type: video/mp4');
-readfile($videoUrl);
+
+header('Content-type: video/mp4');
+header('Content-type: video/mpeg');
+header('Content-disposition: inline');
+header("Content-Transfer-Encoding:­ binary");
+header("Content-Length: ".filesize($out));
+echo $out;
+exit();
+?>
