d2cec85fc3642a2c3a976ddd2e53ef5a1b4de7af
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-16 10:31:40 +0530

    Update download.php

--- download.php
+++ download.php
@@ -1,18 +1,59 @@
 <?php
-$videoUrl = "https://downloadmovie1click.online/Transformers.Rise.Of.The.Beasts.2023.mp4";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $videoUrl);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_HEADER, 0);
-$out = curl_exec($ch);
-curl_close($ch);
+$file = "https://downloadmovie1click.online/Transformers.Rise.Of.The.Beasts.2023.mp4";
+$fp = @fopen($file, 'rb');
 
+$size   = filesize($file); // File size
+$length = $size;           // Content length
+$start  = 0;               // Start byte
+$end    = $size - 1;       // End byte
 
 header('Content-type: video/mp4');
-header('Content-type: video/mpeg');
-header('Content-disposition: inline');
-header("Content-Transfer-Encoding:­ binary");
-header("Content-Length: ".filesize($out));
-echo $out;
+header("Accept-Ranges: 0-$length");
+if (isset($_SERVER['HTTP_RANGE'])) {
+
+    $c_start = $start;
+    $c_end   = $end;
+
+    list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+    if (strpos($range, ',') !== false) {
+        header('HTTP/1.1 416 Requested Range Not Satisfiable');
+        header("Content-Range: bytes $start-$end/$size");
+        exit;
+    }
+    if ($range == '-') {
+        $c_start = $size - substr($range, 1);
+    }else{
+        $range  = explode('-', $range);
+        $c_start = $range[0];
+        $c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+    }
+    $c_end = ($c_end > $end) ? $end : $c_end;
+    if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+        header('HTTP/1.1 416 Requested Range Not Satisfiable');
+        header("Content-Range: bytes $start-$end/$size");
+        exit;
+    }
+    $start  = $c_start;
+    $end    = $c_end;
+    $length = $end - $start + 1;
+    fseek($fp, $start);
+    header('HTTP/1.1 206 Partial Content');
+}
+header("Content-Range: bytes $start-$end/$size");
+header("Content-Length: ".$length);
+
+
+$buffer = 1024 * 8;
+while(!feof($fp) && ($p = ftell($fp)) <= $end) {
+
+    if ($p + $buffer > $end) {
+        $buffer = $end - $p + 1;
+    }
+    set_time_limit(0);
+    echo fread($fp, $buffer);
+    flush();
+}
+
+fclose($fp);
 exit();
 ?>
