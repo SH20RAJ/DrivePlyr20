484950ce910cda43710e415c369b19d9ce6af7d6
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-23 20:12:24 +0530

    ef

---
+++ tos.php
@@ -0,0 +1,112 @@
+
+
+
+<p><strong>Terms of Service (TOS) for DrivePlyr:</strong></p>
+<p>Welcome to DrivePlyr! By using our website, you agree to the following Terms of Service:</p>
+<ol>
+<li><p><strong>Introduction:</strong>
+DrivePlyr is a video sharing platform that allows users to upload, share, and view videos. These Terms of Service govern your use of the DrivePlyr website and any related services.</p>
+</li>
+<li><p><strong>User-Generated Content:</strong>
+All content uploaded to DrivePlyr is user-generated. We do not endorse, guarantee, or take responsibility for the accuracy or legality of any user-uploaded content. Users are solely responsible for the videos they upload.</p>
+</li>
+<li><p><strong>Copyright and Content Guidelines:</strong>
+a. Users must not upload any content that infringes upon the copyrights of others.
+b. Pornographic, sexually explicit, or adult content is strictly prohibited.
+c. Uploading content that violates any local, national, or international laws is prohibited.</p>
+</li>
+<li><p><strong>Intellectual Property:</strong>
+a. DrivePlyr respects the intellectual property rights of others. If you believe that any content on our website infringes your copyright, please follow the steps outlined in our DMCA (Digital Millennium Copyright Act) policy.</p>
+</li>
+<li><p><strong>User Conduct:</strong>
+a. Users must not use DrivePlyr for any illegal or unauthorized purposes.
+b. Harassment, bullying, or threatening behavior towards other users is not tolerated.
+c. Users must not attempt to disrupt or interfere with the website&#39;s functionality or security.</p>
+</li>
+<li><p><strong>Privacy Policy:</strong>
+a. Please refer to our Privacy Policy for information on how we collect, use, and protect your personal data.
+b. By using DrivePlyr, you consent to the practices outlined in our Privacy Policy.</p>
+</li>
+<li><p><strong>Disclaimer of Liability:</strong>
+a. DrivePlyr is provided on an &quot;as is&quot; basis, and we make no warranties, express or implied, regarding the website&#39;s availability, performance, or suitability for your intended use.
+b. We shall not be liable for any damages or losses arising from your use of DrivePlyr or any content uploaded by users.</p>
+</li>
+<li><p><strong>Indemnification:</strong>
+a. By using DrivePlyr, you agree to indemnify and hold harmless DrivePlyr and its affiliates from any claims, damages, or expenses arising from your use of the website or any content you upload.</p>
+</li>
+<li><p><strong>Modification of Terms:</strong>
+a. DrivePlyr reserves the right to modify these Terms of Service at any time. Any changes will be posted on the website, and your continued use of DrivePlyr after such modifications constitutes your acceptance of the updated terms.</p>
+</li>
+<li><p><strong>Termination:</strong>
+a. DrivePlyr may terminate or suspend your account and access to the website if you violate these Terms of Service or engage in any activities that are harmful to the platform or its users.</p>
+</li>
+<li><p><strong>Governing Law:</strong>
+a. These Terms of Service shall be governed by and construed in accordance with the laws of [Your country or state].</p>
+</li>
+</ol>
+<p>If you have any questions or concerns about these Terms of Service, please contact us at [contact@email.com].</p>
+<hr>
+<p><strong>DMCA (Digital Millennium Copyright Act) Policy:</strong></p>
+<p>DrivePlyr respects the intellectual property rights of others and expects its users to do the same. As part of our commitment to copyright protection, we have implemented a DMCA policy. If you believe that any content on DrivePlyr infringes your copyright, please follow the procedure outlined below:</p>
+<ol>
+<li><p><strong>Notification:</strong>
+If you are a copyright owner or an agent thereof, and you believe that any content on DrivePlyr infringes upon your copyrights, you may submit a written notification to our designated Copyright Agent.</p>
+</li>
+<li><p><strong>Information Required:</strong>
+Your DMCA notification should include the following information:
+a. A physical or electronic signature of the copyright owner or a person authorized to act on their behalf.
+b. Identification of the copyrighted work claimed to have been infringed.
+c. Identification of the infringing material, including its location on DrivePlyr.
+d. Your contact information, including your name, address, phone number, and email address.
+e. A statement that you have a good faith belief that the use of the material in the manner complained of is not authorized by the copyright owner, its agent, or the law.
+f. A statement that the information in the notification is accurate, and under penalty of perjury, you are authorized to act on behalf of the copyright owner.</p>
+</li>
+<li><p><strong>Submit the Notification:</strong>
+You can send your DMCA notification to our designated Copyright Agent at [copyright@email.com].</p>
+</li>
+<li><p><strong>Counter-Notification:</strong>
+If you believe that the content you uploaded to DrivePlyr was removed in error or misidentification, you may submit a counter-notification to our designated Copyright Agent.</p>
+</li>
+<li><p><strong>Repeat Infringers:</strong>
+DrivePlyr reserves the right to terminate user accounts that are found to be repeat infringers of copyrights.</p>
+</li>
+<li><p><strong>Contact Information:</strong>
+If you have any questions or concerns about our DMCA policy, please contact our designated Copyright Agent at [copyright@email.com].</p>
+</li>
+</ol>
+<hr>
+<p><strong>Privacy Policy:</strong></p>
+<p>DrivePlyr is committed to protecting your privacy. Our Privacy Policy explains how we collect, use, and safeguard your personal information. Please read the following carefully:</p>
+<ol>
+<li><p><strong>Information We Collect:</strong>
+a. DrivePlyr collects information that you provide when creating an account, such as your name, email address, and password.
+b. We may collect information about your use of the website, including your uploaded videos and interactions with other users.</p>
+</li>
+<li><p><strong>Use of Information:</strong>
+a. We use the information you provide to operate and maintain DrivePlyr, enhance user experience, and improve our services.
+b. Your email address may be used to send you updates, newsletters, or important announcements related to DrivePlyr. You can opt-out of marketing communications at any time.</p>
+</li>
+<li><p><strong>Protection of Information:</strong>
+a. DrivePlyr employs industry-standard security measures to protect your personal information from unauthorized access, disclosure, or alteration.
+b. While we take reasonable precautions, no data transmission over the internet is entirely secure, and we cannot guarantee the security of your information.</p>
+</li>
+<li><p><strong>Third-Party Services:</strong>
+a. DrivePlyr may use third-party services that collect and process user data. Please review the privacy policies of these services for more information.</p>
+</li>
+<li><p><strong>Cookies:</strong>
+a. DrivePlyr uses cookies to enhance user experience and gather information about site usage. You can control cookie preferences through your browser settings.</p>
+</li>
+<li><p><strong>Children&#39;s Privacy:</strong>
+a. DrivePlyr is not intended for use by children under the age of 13. If we become aware that personal information from a child under 13 has been collected, we will take appropriate steps to delete it.</p>
+</li>
+<li><p><strong>Updates to the Privacy Policy:</strong>
+a. DrivePlyr may update this Privacy Policy from time to time. We will notify you of any significant changes via email or by posting a notice on the website.</p>
+</li>
+<li><p><strong>Contact Us:</strong>
+a. If you have any questions or concerns about our Privacy Policy or data practices, please contact us at [privacy@email.com].</p>
+</li>
+</ol>
+<p>By using DrivePlyr, you consent to</p>
+<p> the practices described in this Privacy Policy.</p>
+<hr>
+<p>Please note that the provided templates are for informational purposes only and may need to be reviewed and adapted to comply with the specific laws and regulations of your jurisdiction. It is strongly advised to consult with legal professionals to ensure your Terms of Service, DMCA policy, and Privacy Policy accurately and comprehensively reflect the legal requirements applicable to your video sharing website, DrivePlyr.</p>
