70257e3baabffc21cdf7f8f829f76c2f4a689e89
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-23 17:36:48 +0530

    ky

--- dashboard/nav.php
+++ dashboard/nav.php
@@ -3,7 +3,7 @@
     <div class="scrollbar-inner">
       <!-- Brand -->
       <div class="sidenav-header  align-items-center">
-        <a class="navbar-brand" href="javascript:void(0)">
+        <a class="navbar-brand" href="../../">
           <img src="../dp.png" class="navbar-brand-img" alt="DrivePlyr Logo">
         </a>
       </div>
