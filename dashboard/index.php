d79f2f0eea46f242d99ebec473bc21c97e4ad093
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-23 19:04:51 +0530

    dw

--- dashboard/index.php
+++ dashboard/index.php
@@ -17,7 +17,9 @@ if ($result) {
 } else {
     echo "Error executing query: " . mysqli_error($connection);
 }
-
+$num_users = mysqli_query($connection, "SELECT COUNT(*) AS num_users FROM users");
+$num_users = mysqli_fetch_assoc($result);
+$num_users = $num_users['num_users'];
 ?>
 
 <!DOCTYPE html>
@@ -87,7 +89,7 @@ if ($result) {
                   <div class="row">
                     <div class="col">
                       <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
-                      <span class="h2 font-weight-bold mb-0">2,356</span>
+                      <span class="h2 font-weight-bold mb-0"><?php echo $num_users ?></span>
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
