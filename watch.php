a22094ed73761bcef2f5d1299783f35db9a5b73c
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-23 17:18:15 +0530

    er

--- watch.php
+++ watch.php
@@ -38,7 +38,7 @@ die();*/
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>DrivePlyr</title>
+  <title><?php echo $videoTitle ?> - DrivePlyr</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lux/bootstrap.min.css">
 </head>
 <body>
