d7a12f631508a492ae98066e8a3e06ffa228299b
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-24 14:55:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,10 +17,19 @@ include 'func.php';
     <link rel="manifest" href="manifest.json">
   
     <script>
-  if (typeof navigator.serviceWorker !== 'undefined') {
-    navigator.serviceWorker.register('sw.js')
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./sw.js')
+        .then((registration) => {
+          console.log('ServiceWorker registration successful with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.log('ServiceWorker registration failed:', error);
+        });
+    });
   }
-    </script>
+</script>
+
   </head>
   <body>
     <!-- Header Starts -->
