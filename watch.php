3346d89518fd2b6269b7cc21dff746f9c5625dbf
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-26 23:19:45 +0530

    xsaxa

--- watch.php
+++ watch.php
@@ -206,6 +206,76 @@ $query = 'UPDATE videos SET views = views + 1 WHERE id = '. $id .'';
   </code>
 </pre>
 <!-- ShareThis BEGIN --><div class="sharethis-inline-share-buttons"></div><!-- ShareThis END -->
+<!-- Facebook -->
+<a class="btn btn-primary" style="background-color: #3b5998;" href="#!" role="button"
+  ><i class="fab fa-facebook-f"></i
+></a>
+
+<!-- Twitter -->
+<a class="btn btn-primary" style="background-color: #55acee;" href="#!" role="button"
+  ><i class="fab fa-twitter"></i
+></a>
+
+<!-- Google -->
+<a class="btn btn-primary" style="background-color: #dd4b39;" href="#!" role="button"
+  ><i class="fab fa-google"></i
+></a>
+
+<!-- Instagram -->
+<a class="btn btn-primary" style="background-color: #ac2bac;" href="#!" role="button"
+  ><i class="fab fa-instagram"></i
+></a>
+
+<!-- Linkedin -->
+<a class="btn btn-primary" style="background-color: #0082ca;" href="#!" role="button"
+  ><i class="fab fa-linkedin-in"></i
+></a>
+
+<!-- Pinterest -->
+<a class="btn btn-primary" style="background-color: #c61118;" href="#!" role="button"
+  ><i class="fab fa-pinterest"></i
+></a>
+
+<!-- Vkontakte -->
+<a class="btn btn-primary" style="background-color: #4c75a3;" href="#!" role="button"
+  ><i class="fab fa-vk"></i
+></a>
+
+<!-- Stack overflow -->
+<a class="btn btn-primary" style="background-color: #ffac44;" href="#!" role="button"
+  ><i class="fab fa-stack-overflow"></i
+></a>
+
+<!-- Youtube -->
+<a class="btn btn-primary" style="background-color: #ed302f;" href="#!" role="button"
+  ><i class="fab fa-youtube"></i
+></a>
+
+<!-- Slack -->
+<a class="btn btn-primary" style="background-color: #481449;" href="#!" role="button"
+  ><i class="fab fa-slack-hash"></i
+></a>
+
+<!-- Github -->
+<a class="btn btn-primary" style="background-color: #333333;" href="#!" role="button"
+  ><i class="fab fa-github"></i
+></a>
+
+<!-- Dribbble -->
+<a class="btn btn-primary" style="background-color: #ec4a89;" href="#!" role="button"
+  ><i class="fab fa-dribbble"></i
+></a>
+
+<!-- Reddit -->
+<a class="btn btn-primary" style="background-color: #ff4500;" href="#!" role="button"
+  ><i class="fab fa-reddit-alien"></i
+></a>
+
+<!-- Whatsapp -->
+<a class="btn btn-primary" style="background-color: #25d366;" href="#!" role="button"
+  ><i class="fab fa-whatsapp"></i
+></a>
+
         </div>
 
         <!-- Modal footer -->
