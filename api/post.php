5e45bef41b3f1bc4ef99b0ce7dc1ab18879da2a5
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 14:37:03 +0530

    dw

--- api/post.php
+++ api/post.php
@@ -1,13 +1,26 @@
 <?php
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get all POST data and store it in an array
-    $postData = $_POST;
+    // Get the JSON data sent from the client-side
+    $jsonData = file_get_contents('php://input');
 
-    // Convert the array to JSON and echo it
-    echo json_encode($postData);
+    // Convert the JSON data to an associative array
+    $data = json_decode($jsonData, true);
+
+    // Validate data (you should implement proper validation based on your requirements)
+
+    // Perform further actions with the user details as needed
+    // For this example, we'll simply log the data
+    // Replace this with your desired logic, such as saving the data to a database
+    // or using it for user authentication
+
+    // Log the received data
+    file_put_contents('received_data.txt', print_r($data, true));
+
+    // Send a response to the client
+    echo json_encode(array('success' => true, 'message' => 'Data received successfully.'));
 } else {
     // Invalid request method
-    echo json_encode(array('error' => 'Invalid request method.'));
+    echo json_encode(array('success' => false, 'message' => 'Invalid request method.'));
 }
 ?>
