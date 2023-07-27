24151a49d608912ab391688a312d7d2f4abfe6f6
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 14:17:20 +0530

    qw

---
+++ api/test.php
@@ -0,0 +1,10 @@
+<?php
+// Sample data for testing
+$_POST['email'] = 'testr@gmail.com';
+$_POST['fullName'] = 'John Doe';
+$_POST['profilePicture'] = 'https://example.com/avatar.jpg';
+
+// Include the conn.php file and google.php
+require_once '../conn.php';
+require_once 'google.php';
+?>
