7176dc158373c3aaed0309b938b9efcff8c804e9
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 20:46:08 +0530

    uihud

--- watch.php
+++ watch.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include 'conn.php';
 include 'func.php';
 // Check if the user is logged in
