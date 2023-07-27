4c25781e640a9c5705fb4eb4a1f93f02f6df09b6
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 13:11:36 +0530

    dw3e3

--- dashboard/profile.php
+++ dashboard/profile.php
@@ -46,7 +46,6 @@ function isDriveLink(url) {
     <?php include 'topnav.php' ?>
     <!-- Page content -->
     <div class="container-fluid mt-6">
-      <div class="row">
         <div class="col-xl-4 order-xl-2">
                   <div class="card">
                     <div class="card-body text-center">
@@ -82,7 +81,6 @@ function isDriveLink(url) {
                       </div>
                     </div>
                   </div>
-                </div>
 
         </div>
         <div class="col-xl-8 order-xl-1">
