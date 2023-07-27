5676a65162c4cc6fb0240ecec67a2e44088dabdf
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 15:15:05 +0530

    vfd

--- dashboard/register.php
+++ dashboard/register.php
@@ -1,10 +1,10 @@
 
   <?php 
   session_start();
-  if(isset($_SESSION['id'])){
-    header('Location: ../dashboard/');
-    exit;
-  }
+  // if(isset($_SESSION['id'])){
+  //   header('Location: ../dashboard/');
+  //   exit;
+  // }
   include 'head.php' 
   
   ?>
