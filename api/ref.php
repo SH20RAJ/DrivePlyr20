71effa36f4e16f58f24f78f9b485b3cc03e1214f
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 21:51:59 +0530

    xa

--- api/ref.php
+++ api/ref.php
@@ -1,23 +1,3 @@
 <?php
 // ref.php
 include '../conn.php';
-// Check if the "ref" parameter is provided in the URL
-if (isset($_GET['ref'])) {
-    // Retrieve the "ref" parameter value
-    $refValue = $_GET['ref'];
-
-    // Get user IP address
-    $userIP = $_SERVER['REMOTE_ADDR'];
-    // Get current timestamp
-    $timestamp = time();
-
-    // Get user agent (browser) details
-    $userAgent = $_SERVER['HTTP_USER_AGENT'];
-
-
-    // SQL query to insert data into the 'ref' table
-    echo $sql = "INSERT INTO ref (ref_value, user_ip, timestamp, user_agent) VALUES ('$refValue', '$userIP', '$timestamp', '$userAgent')";
-
-    $conn->query($sql)
-}
-?>
