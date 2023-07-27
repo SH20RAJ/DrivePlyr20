521c522ee1790cbbf3f6648064f291bd221eedb0
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 14:04:19 +0530

    y

--- api/google.php
+++ api/google.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once '../conn.php';
+require_once 'conn.php';
 
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -12,30 +12,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Validate data (you should implement proper validation based on your requirements)
 
-    // Set the username based on the email (use email without the domain for all emails except 'xyz@gmail.com')
-    $domain = explode('@', $email)[1]; // Get the domain part of the email
-    $username = ($domain === 'gmail.com') ? explode('@', $email)[0] : '';
+    // Check if the email exists in the 'users' table
+    $selectQuery = "SELECT * FROM users WHERE email = ?";
+    $stmt = $conn->prepare($selectQuery);
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
 
-    // Convert the avatar URL to remove the "=s96-c" part
-    $profilePicture = preg_replace('/=s\d+-c$/', '', $profilePicture);
-
-    // Insert the data into the 'users' table
-    $insertQuery = "INSERT INTO users (name, username, email, avatar, date, ip) VALUES (?, ?, ?, ?, NOW(), ?)";
-    $stmt = $conn->prepare($insertQuery);
-    $stmt->bind_param('sssss', $fullName, $username, $email, $profilePicture, $ip);
-
-    if ($stmt->execute()) {
-        // Data insertion successful
+    if ($user) {
+        // User with this email already exists, log in the user
 
         // Set session variables
-        $_SESSION['id'] = $stmt->insert_id; // Get the inserted user's ID
-        $_SESSION['username'] = $username;
-        $_SESSION['name'] = $fullName;
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['name'] = $user['name'];
 
-        echo json_encode(array('success' => true, 'message' => 'Data added to database.'));
+        echo json_encode(array('success' => true, 'message' => 'User logged in successfully.'));
     } else {
-        // Data insertion failed
-        echo json_encode(array('success' => false, 'message' => 'Failed to add data to database.'));
+        // User does not exist, insert the data into the 'users' table
+
+        // Set the username based on the email (use email without the domain for all emails except 'xyz@gmail.com')
+        $domain = explode('@', $email)[1]; // Get the domain part of the email
+        $username = ($domain === 'gmail.com') ? explode('@', $email)[0] : '';
+
+        // Convert the avatar URL to remove the "=s96-c" part
+        $profilePicture = preg_replace('/=s\d+-c$/', '', $profilePicture);
+
+        // Insert the data into the 'users' table
+        $insertQuery = "INSERT INTO users (name, username, email, avatar, date, ip) VALUES (?, ?, ?, ?, NOW(), ?)";
+        $stmt = $conn->prepare($insertQuery);
+        $stmt->bind_param('sssss', $fullName, $username, $email, $profilePicture, $ip);
+
+        if ($stmt->execute()) {
+            // Data insertion successful
+
+            // Set session variables
+            $_SESSION['id'] = $stmt->insert_id; // Get the inserted user's ID
+            $_SESSION['username'] = $username;
+            $_SESSION['name'] = $fullName;
+
+            echo json_encode(array('success' => true, 'message' => 'New user registered and logged in.'));
+        } else {
+            // Data insertion failed
+            echo json_encode(array('success' => false, 'message' => 'Failed to add data to database.'));
+        }
     }
 
     $stmt->close();
