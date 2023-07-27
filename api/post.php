7c947f3cc9a1f7a16221f1dfde9c49026734d630
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 14:41:08 +0530

    y45

--- api/post.php
+++ api/post.php
@@ -8,17 +8,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode($jsonData, true);
 
     // Validate data (you should implement proper validation based on your requirements)
+    $email = $data['email'] ?? '';
+    $fullName = $data['fullName'] ?? '';
+    $profilePicture = $data['profilePicture'] ?? '';
 
     // Perform further actions with the user details as needed
-    // For this example, we'll simply log the data
-    // Replace this with your desired logic, such as saving the data to a database
-    // or using it for user authentication
+    // For this example, we'll simply echo the data
 
-    // Log the received data
-    print_r($data, true);
+    // Echo the individual data fields as a response to the client
+    echo 'Email: ' . $email . '<br>';
+    echo 'Full Name: ' . $fullName . '<br>';
+    echo 'Profile Picture: ' . $profilePicture . '<br>';
 
     // Send a response to the client
+    echo json_encode(array('success' => true, 'message' => 'Data received successfully.'));
 } else {
     // Invalid request method
+    echo json_encode(array('success' => false, 'message' => 'Invalid request method.'));
 }
 ?>
