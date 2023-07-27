9977d6136aa47b5add6cf50f49fe6cac9be57aed
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 12:46:05 +0530

    sa

--- dashboard/profile.php
+++ dashboard/profile.php
@@ -39,13 +39,7 @@
     </div>
     <!-- Page content -->
     <div class="container-fluid mt--6">
-      <div class="row">
-        <div class="col">
-          <div class="card border-0">
-            <div id="map-default" class="map-canvas" data-lat="40.748817" data-lng="-73.985428" style="height: 600px;"></div>
-          </div>
-        </div>
-      </div>
+      
       <!-- Footer -->
       <?php include 'footer.php' ?>
     </div>
