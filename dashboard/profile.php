8fed9f70353ced51ebc92aa7dea9ca85b53e5e59
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 13:16:26 +0530

    ty

--- dashboard/profile.php
+++ dashboard/profile.php
@@ -18,66 +18,43 @@
     <div class="container-fluid mt-6">
       <div class="row">
         <div class="col-xl-4 order-xl-2">
-        <!DOCTYPE html>
+          
+                  <div class="container mt-5">
+                    <div class="card">
+                      <div class="card-body text-center">
+                        <!-- Circular logo with background image -->
+                        <div class="user-logo mx-auto mb-4">
+                          <!-- Replace 'logo.png' with the actual logo image URL -->
+                          <img src="logo.png" alt="Logo">
+                        </div>
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<style>
-  /* Custom CSS for the circular logo with background image */
-  .user-logo {
-    width: 150px;
-    height: 150px;
-    border-radius: 50%;
-    background-color: #f2f2f2;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-    background-image: url('background.jpg'); /* Replace 'background.jpg' with the actual background image URL */
-    background-size: cover;
-  }
-  .user-logo img {
-    max-width: 100%;
-    max-height: 100%;
-  }
-</style>
-</head>
-<body>
-<div class="container mt-5">
-  <div class="card">
-    <div class="card-body text-center">
-      <!-- Circular logo with background image -->
-      <div class="user-logo mx-auto mb-4">
-        <!-- Replace 'logo.png' with the actual logo image URL -->
-        <img src="logo.png" alt="Logo">
-      </div>
+                        <!-- User Name -->
+                        <h4 class="card-title">John Doe</h4>
 
-      <!-- User Name -->
-      <h4 class="card-title">John Doe</h4>
+                        <!-- Website -->
+                        <p class="card-text"><a href="https://www.example.com" target="_blank">www.example.com</a></p>
 
-      <!-- Website -->
-      <p class="card-text"><a href="https://www.example.com" target="_blank">www.example.com</a></p>
+                        <!-- Follow button (you can replace '#' with the follow action URL) -->
+                        <button class="btn btn-primary mb-3" onclick="followUser('#')">Follow</button>
 
-      <!-- Follow button (you can replace '#' with the follow action URL) -->
-      <button class="btn btn-primary mb-3" onclick="followUser('#')">Follow</button>
-
-      <!-- Row with number of followers, views, and videos -->
-      <div class="row text-center">
-        <div class="col">
-          <h5>100</h5>
-          <p>Followers</p>
-        </div>
-        <div class="col">
-          <h5>500</h5>
-          <p>Views</p>
-        </div>
-        <div class="col">
-          <h5>50</h5>
-          <p>Videos</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+                        <!-- Row with number of followers, views, and videos -->
+                        <div class="row text-center">
+                          <div class="col">
+                            <h5>100</h5>
+                            <p>Followers</p>
+                          </div>
+                          <div class="col">
+                            <h5>500</h5>
+                            <p>Views</p>
+                          </div>
+                          <div class="col">
+                            <h5>50</h5>
+                            <p>Videos</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
 
 
 
