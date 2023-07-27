18a147922d84c380ba909ce16421a28c9badaccd
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-27 14:08:20 +0530

    u65

--- api/google.php
+++ api/google.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-require_once 'conn.php';
+require_once '../conn.php';
 
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
