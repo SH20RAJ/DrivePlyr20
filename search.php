7c7beac4e964a189071c3c1a42c2e63b3d0b78b7
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-28 15:40:53 +0530

    uh

--- search.php
+++ search.php
@@ -122,7 +122,7 @@ include 'func.php';
       <!-- Videos Section -->
       <div class="videos">
      <center>
-      <div class="">
+      <div class="header__search" style="display:block;">
       <form action="search.php">
           <input name="q" type="text" placeholder="Search" value="<?php echo $_GET['q'] ?>"/>
           <button type="submit"><i class="material-icons">search</i></button>
