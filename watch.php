2f5e23e5f5226250a3f4756d3e41a3bb5fb077fb
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-28 13:39:33 +0530

    t54

--- watch.php
+++ watch.php
@@ -96,7 +96,25 @@ die();*/
               <button class="btn btn-sm btn-outline-danger">Messege</button>
             </div>
           </div>
-          
+          <div id="disqus_thread"></div>
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://driveplyr.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
           <script id="dsq-count-scr" src="//driveplyr.disqus.com/count.js" async></script>
           
           <p><?php echo $videoDescription ?></p>
