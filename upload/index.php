0ec07ec0c8bf47e547da77dd47f52dbd5ac5143a
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-28 07:28:39 +0000

    ok

--- upload/index.php
+++ upload/index.php
@@ -1,72 +1,44 @@
+<?php
+require 'vendor/autoload.php';
 
-    <?php
-// Function to get the size of the file to calculate progress
-function getFileSize($filePath) {
-    return filesize($filePath);
+use GuzzleHttp\Client;
+
+function uploadFileToGitHub($filePath, $token, $repositoryOwner, $repositoryName)
+{
+    // Read the file as binary data
+    $fileContent = file_get_contents($filePath);
+
+    // Set up the request headers, including the GitHub access token
+    $headers = [
+        'Authorization' => 'Bearer ' . $token,
+        'Content-Type' => 'application/octet-stream',
+    ];
+
+    // Compose the API endpoint for creating a new release asset
+    $uploadUrl = "https://api.github.com/repos/{$repositoryOwner}/{$repositoryName}/releases/latest/assets?name=" . urlencode(basename($filePath));
+
+    // Make the API request to upload the file using cURL
+    $client = new Client();
+    $response = $client->request('POST', $uploadUrl, [
+        'headers' => $headers,
+        'body' => $fileContent,
+    ]);
+
+    $statusCode = $response->getStatusCode();
+    if ($statusCode === 201) {
+        echo 'File uploaded successfully!' . PHP_EOL;
+        $data = json_decode($response->getBody(), true);
+        echo 'Download URL: ' . $data['browser_download_url'] . PHP_EOL;
+    } else {
+        echo 'Error uploading file. Status Code: ' . $statusCode . PHP_EOL;
+        echo 'Response: ' . $response->getBody() . PHP_EOL;
+    }
 }
 
-// Function to handle the progress updates during file upload
-function handleProgress($resource, $download_size, $downloaded, $upload_size, $uploaded) {
-    // Calculate and update the progress percentage
-    $progress = ($upload_size > 0) ? ($uploaded / $upload_size) * 100 : 0;
+// Example usage:
+$filePath = 'token.txt';
+$token = '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';
+$repositoryOwner = 'sh20raj';
+$repositoryName = 'cdns20';
 
-    // Send progress to the client using JavaScript
-    echo '<script>';
-    echo 'document.getElementById("progressBar").value = ' . intval($progress) . ';';
-    echo 'document.getElementById("progressLabel").innerText = "' . intval($progress) . '%";';
-    echo '</script>';
-    // Flush the output buffer to ensure the progress is sent immediately
-    ob_flush();
-    flush();
-}
-
-// Replace these variables with your own values
-$token = "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";
-$repositoryOwner = "jokes4ush";
-$repositoryName = "music";
-$filePath = "token.txt";
-
-$ch = curl_init("https://api.github.com/repos/$repositoryOwner/$repositoryName/contents/$filePath");
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    "Authorization: Bearer $token",
-    "Content-Type: application/json"
-));
-
-$fileContent = file_get_contents($filePath);
-$fileContentEncoded = base64_encode($fileContent);
-
-$data = json_encode(array(
-    "message" => "txt file",
-    "content" => $fileContentEncoded
-));
-
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_NOPROGRESS, false);
-curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, 'handleProgress');
-curl_setopt($ch, CURLOPT_INFILE, fopen($filePath, 'rb'));
-curl_setopt($ch, CURLOPT_INFILESIZE, getFileSize($filePath));
-
-$result = curl_exec($ch);
-curl_close($ch);
-
-// Decode the result to get the uploaded file details
-$responseData = json_decode($result, true);
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Upload File to GitHub</title>
-</head>
-<body>
-    <h1>Upload a File to GitHub</h1>
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="file" name="file" />
-        <button type="submit">Upload</button>
-    </form>
-    <progress id="progressBar" value="0" max="100"></progress>
-    <span id="progressLabel">0%</span>
-</body>
-</html>
+uploadFileToGitHub($filePath, $token, $repositoryOwner, $repositoryName);
