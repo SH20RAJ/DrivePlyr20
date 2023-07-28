70106d7c3075d436116516cb08860143770c0c9d
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-28 07:05:54 +0530

    Update google.php

--- api/google.php
+++ api/google.php
@@ -62,6 +62,40 @@ $stmt->bind_param('sssss', $fullName, $username, $email, $profilePicture, $_SERV
         echo json_encode(array('success' => false, 'message' => 'Failed to add data to database.'));
     }
 }
+//<?php
+// API endpoint URL
+$url = 'https://getintotouch.sh20raj.com/api.php?id=1479193538';
+
+// Data to send in the POST request
+//$data = array(
+ //   'New User Registration on DrivePlyr' => 'sh20raj',
+ //   'param2' => 'value2',
+    // Add more parameters and their values as needed
+//);
+
+// Initialize cURL session
+$curl = curl_init();
+
+// Set the cURL options
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+// Execute cURL session and store the response
+$response = curl_exec($curl);
+
+// Check for cURL errors
+if (curl_errno($curl)) {
+  //  echo 'cURL Error: ' . curl_error($curl);
+}
+
+// Close cURL session
+curl_close($curl);
+
+// Output the API response
+//echo $response;
+//?
 
 $stmt->close();
 $conn->close();
