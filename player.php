ab411a7260e5f8d835eee286eaa7f59f2ff330b0
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-29 18:54:40 +0530

    jgh

--- player.php
+++ player.php
@@ -18,7 +18,7 @@ $video = json_decode(file_get_contents('https://driveplyr.appspages.online/dashb
 include 'conn.php';
 $query = 'UPDATE videos SET views = views + 1 WHERE id = '. $_GET['id'] .'';
 $result = $conn->query($query);   
-echo '<img src="https://iplogger.com/driveplyrplayer" ></img>';
+echo '<img src="https://iplogger.com/driveplyrplayer" style="display:none;"></img>';
 include 'dashboard/tracker.php';
 
 ?>
