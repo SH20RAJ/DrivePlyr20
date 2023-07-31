1bf8e62e6c9cf56e607b73e7660ec507ba834588
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-31 19:25:09 +0530

    we

---
+++ dashboard/api/ads.php
@@ -0,0 +1,38 @@
+<?php
+
+include '../../conn.php';
+// Function to sanitize input data to prevent SQL injection
+function sanitize_input($data)
+{
+    global $conn;
+    return mysqli_real_escape_string($conn, $data);
+}
+
+// Check if the form is submitted and update user data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize input data
+    $preRollURL = sanitize_input($_POST['pre_roll_url']);
+    $midRollURL = sanitize_input($_POST['mid_roll_url']);
+    $postRollURL = sanitize_input($_POST['post_roll_url']);
+    $pauseRollURL = sanitize_input($_POST['pause_roll_url']);
+
+    // Update the data in the users table (assuming you have a primary key, such as user_id)
+    $user_id = 1; // Replace 1 with the actual user_id for whom you want to update the URLs
+    $sql = "UPDATE users SET 
+                pre_roll_url = '$preRollURL', 
+                mid_roll_url = '$midRollURL', 
+                post_roll_url = '$postRollURL', 
+                pause_roll_url = '$pauseRollURL' 
+            WHERE user_id = $user_id";
+
+    if ($conn->query($sql) === TRUE) {
+        // Data updated successfully
+        echo "URLs updated successfully";
+    } else {
+        // Error occurred while updating data
+        echo "Error updating URLs: " . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
