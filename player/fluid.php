636fff107e12d15fdf384a1ce2e68d8172876a55
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-08-01 10:35:17 +0530

    ok

--- player/fluid.php
+++ player/fluid.php
@@ -1,56 +1,80 @@
-<title><?php echo $title ?></title>
-<link rel="icon" href="../logo.png">
-<style>
-  body {
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-  }
-</style>
-<script src="https://cdn.fluidplayer.com/v3/current/fluidplayer.min.js"></script>
-<video poster="<?php echo $poster_url ?>" width="100%" id="video-id">
-  <source src="<?php echo $videourl ?>" type="video/mp4" />
-</video>
-
-<script>
-  var myFP = fluidPlayer(
-    'video-id',
-    {
-      "layoutControls": {
-        // Controls configuration...
-      },
-      <?php 
-      if($monetization){
-        echo '
-          "vastOptions": {
-            "adList": [
-              {
-                "roll": "preRoll",
-                "vastTag": "' . $preRollURL . '",
-                "adText": ""
-              },
-              {
-                "roll": "midRoll",
-                "vastTag": "' . $midRollURL . '",
-                "adText": ""
-              },
-              {
-                "roll": "postRoll",
-                "vastTag": "' . $postRollURL . '",
-                "adText": ""
-              },
-              {
-                "roll": "onPauseRoll",
-                "vastTag": "' . $PauseRollURL . '",
-                "adText": ""
-              }
-            ],
-            "adCTAText": false,
-            "adCTATextPosition": ""
-          }
-        ';
-      }
-      ?>
-    }
-  );
-</script>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $title ?></title>
+    <link rel="icon" href="../logo.png">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+    </style>
+    <script src="https://cdn.fluidplayer.com/v3/current/fluidplayer.min.js"></script>
+</head>
+<body>
+    <video poster="<?php echo $poster_url ?>" width="100%" id="video-id">
+        <source src="<?php echo $videourl ?>" type="video/mp4" />
+    </video>
+	<script>
+    var myFP = fluidPlayer(
+        'video-id',	{
+	"layoutControls": {
+		"controlBar": {
+			"autoHideTimeout": 3,
+			"animated": true,
+			"autoHide": false
+		},
+		"htmlOnPauseBlock": {
+			"html": null,
+			"height": null,
+			"width": null
+		},
+		"autoPlay": true,
+		"mute": false,
+		"allowTheatre": true,
+		"playPauseAnimation": true,
+		"playbackRateEnabled": true,
+		"allowDownload": true,
+		"playButtonShowing": true,
+		"fillToContainer": true,
+		"primaryColor": "blue",
+		"posterImage": ""
+	},
+                <?php 
+                if($monetization){
+                    echo '
+                    "vastOptions": {
+                        "adList": [
+                            {
+                                "roll": "preRoll",
+                                "vastTag": "' . $preRollURL . '",
+                                "adText": ""
+                            },
+                            {
+                                "roll": "midRoll",
+                                "vastTag": "' . $midRollURL . '",
+                                "adText": ""
+                            },
+                            {
+                                "roll": "postRoll",
+                                "vastTag": "' . $postRollURL . '",
+                                "adText": ""
+                            },
+                            {
+                                "roll": "onPauseRoll",
+                                "vastTag": "' . $PauseRollURL . '",
+                                "adText": ""
+                            }
+                        ],
+                        "adCTAText": false,
+                        "adCTATextPosition": ""
+                    }
+                    ';
+                }
+                ?>
+            }
+        );
+    </script>
+</body>
+</html>
