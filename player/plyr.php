6e7aa4ae7f1732dcde0dcecb41b157a80d7ed2e2
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-08-11 10:56:56 +0530

    ok

--- player/plyr.php
+++ player/plyr.php
@@ -1,70 +1,56 @@
-
-<link rel="icon" href="drive-logo.png">
-<title><?php echo $title ?></title>
-<style>
-html, body {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-.plyr  {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  /*object-fit: cover;
-  z-index: -1;*/
-}
-</style>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/CDNSFree2/Plyr/plyr.css" />
-    <?php
+<?php
 function is_youtube_url($url) {
     return preg_match('/youtube\.com\/watch\?/', $url) || preg_match('/youtu\.be\//', $url);
 }
-?>
 
-    <div id="container">
-        <?php
-        if (is_youtube_url($videourl)) {
-            echo '<div class="plyr__video-embed" id="player">';
-            echo get_youtube_embed_code($videourl);
-            echo '</div>';
-        } else {
-            echo '<video controls poster="' . $poster_url . '" id="vid1">';
-            echo '<source src="' . $videourl . '" type="video/mp4" size="576" />';
-            echo '</video>';
-        }
-        ?>
-    </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/plyr/3.6.7/plyr.min.js"></script>
-    <script>
-        var controls = [
-            // Your existing controls here
-        ];
-
-        const player = new Plyr(isYouTubeVideo() ? '#player' : '#vid1', { controls });
-
-        function is_youtube_url(url) {
-            return /youtube\.com\/watch\?/.test(url) || /youtu\.be\//.test(url);
-        }
-
-        function get_youtube_embed_code(url) {
-            var videoId = url.split('v=')[1];
-            return '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
-        }
-    </script>
-
-
-<style>
-  :root {
-  --plyr-color-main: #e657ff;
-    --plyr-video-control-color  :#e8ffba;
+function get_youtube_embed_code($url) {
+    $videoId = explode('v=', $url)[1];
+    return '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
 }
 
-</style>
+$controls = [
+    'play-large',
+    'play',
+    'fast-forward',
+    'progress',
+    'current-time',
+    'duration',
+    'mute',
+    'volume',
+    'captions',
+    'settings',
+    'pip',
+    'airplay',
+];
 
+echo '<link rel="icon" href="drive-logo.png">';
+echo '<title>' . $title . '</title>';
+echo '<style>';
+// Your CSS styles here
+echo '</style>';
+echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/CDNSFree2/Plyr/plyr.css" />';
 
+echo '<div id="container">';
+if (is_youtube_url($videourl)) {
+    echo '<div class="plyr__video-embed" id="player">';
+    echo get_youtube_embed_code($videourl);
+    echo '</div>';
+} else {
+    echo '<video controls poster="' . $poster_url . '" id="vid1">';
+    echo '<source src="' . $videourl . '" type="video/mp4" size="576" />';
+    echo '</video>';
+}
+echo '</div>';
+
+echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/plyr/3.6.7/plyr.min.js"></script>';
+echo '<script>';
+echo 'var controls = ' . json_encode($controls) . ';';
+echo 'const player = new Plyr(' . (is_youtube_url($videourl) ? '\'#player\'' : '\'#vid1\'') . ', { controls });';
+echo '</script>';
+echo '<style>';
+echo ':root {';
+echo '--plyr-color-main: #e657ff;';
+echo '--plyr-video-control-color: #e8ffba;';
+echo '}';
+echo '</style>';
+?>
