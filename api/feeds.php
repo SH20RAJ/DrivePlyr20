cfd70ed188378332a852c32c2c55a6dceae14a73
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-09-14 22:55:29 +0530

    ...

--- api/feeds.php
+++ api/feeds.php
@@ -1,5 +1,9 @@
 <?php
 session_start(); 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include "../conn.php";
 // Retrieve the video list from the database
 // $user = $_SESSION['id'];
