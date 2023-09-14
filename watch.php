e718f2c5a664288155427913fd3927e87ea35987
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-09-14 23:34:10 +0530

    sw

--- watch.php
+++ watch.php
@@ -52,7 +52,8 @@ die();*/
 <meta name="author" content="Shade">
   <link rel="shortcut icon" href="../../logo.png" type="image/x-icon">
   <title><?php echo $videoTitle ?> - DrivePlyr</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lux/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  
   <link rel="stylesheet" href="../../assets/watch.css">
   <link rel="stylesheet" media="screen and (max-width: 600px)" href="../../assets/watch2.css">
 
