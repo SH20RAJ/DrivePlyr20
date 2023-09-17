71f889c46c4b6013b0a9cd45059dd25c655074f0
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-09-17 18:14:27 +0000

    that is good

--- api/videos.php
+++ api/videos.php
@@ -1,5 +1,4 @@
 <?php
-
 // Allow cross-origin requests
 header("Access-Control-Allow-Origin: *"); // Replace '*' with the specific origin(s) you want to allow
 
@@ -9,18 +8,17 @@ header("Content-Type: application/json");
 include "../conn.php";
 include "../func.php";
 
-
-$sql = $_GEt['sql'];
-$limit = isset($_GET['limit'])?$_GET['limit']:20;
-$sql = isset($sql) ? $sql : "SELECT * FROM videos ORDER BY RAND() LIMIT ".$limit."";
+$limit = isset($_GET['limit']) ? $_GET['limit']:20; // Set limit to 1 to get only one result
+$sql = isset($_GET['sql']) ? $_GET['sql'] : "SELECT * FROM videos ORDER BY RAND() LIMIT " . $limit;
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // Loop through each video and generate the table rows
+    // Fetch and encode the first row as JSON
     $row = $result->fetch_assoc();
-
     echo json_encode($row);
+} else {
+    // No results found
+    echo json_encode(array("message" => "No results found"));
 }
-
-
+?>
