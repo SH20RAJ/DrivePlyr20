caa8ad963e9cf4cff5ea95bcd87c587b192ec084
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-09-17 18:17:28 +0000

    op

--- api/videos.php
+++ api/videos.php
@@ -8,17 +8,19 @@ header("Content-Type: application/json");
 include "../conn.php";
 include "../func.php";
 
-$limit = isset($_GET['limit']) ? $_GET['limit']:20; // Set limit to 1 to get only one result
+$limit = isset($_GET['limit']) ? $_GET['limit'] : 20; // Set the limit to 20 results
 $sql = isset($_GET['sql']) ? $_GET['sql'] : "SELECT * FROM videos ORDER BY RAND() LIMIT " . $limit;
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // Fetch and encode the first row as JSON
-    $row = $result->fetch_assoc();
-    echo json_encode($row);
+    $rows = array();
+    while ($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
+    echo json_encode($rows, JSON_PRETTY_PRINT);
 } else {
     // No results found
-    echo json_encode(array("message" => "No results found"));
+    echo json_encode(array("message" => "No results found"), JSON_PRETTY_PRINT);
 }
 ?>
