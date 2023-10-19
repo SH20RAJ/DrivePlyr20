681f861d2ec5c92a914204847811a998b8d2d062
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-10-19 12:16:35 +0000

    skysports

---
+++ xcv.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>  
+<html lang="en">
+<head>
+<link rel="icon" href="" type="image/gif" sizes="16x16"> 
+<meta charset="UTF-8">
+<title> sky sports</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="//content.jwplatform.com/libraries/SAHhwvZq.js"></script>
+<style>
+body {
+    
+margin: 0px  
+}
+
+.jwplayer {
+position: absolute !important
+}
+
+.jwplayer.jw-flag-aspect-mode {
+min-height: 32%;  
+max-height: 32%
+}
+jwplayer.setup({"logo": {
+"file": "",
+"link": "",}}
+);
+</style>
+</head>
+<body>
+<div id="jwplayerDiv"></div>  
+<script type="text/javascript">
+jwplayer("jwplayerDiv").setup({
+file: "https://linear003-gb-dash1-prd-cf.cdnskycdp.com/016a/Content/DASH_003_sd/Live/channel(skysportscricke)/manifest_sd.mpd", position: 'bottom',   
+type: "dash", drm: {"clearkey": {    
+"keyId": "0032f72d21efd159d45a3a74decd84",     
+"key": "df0ec405864d1f3d4728ab5fd62adc3"     
+}    
+}
+}); </script>
